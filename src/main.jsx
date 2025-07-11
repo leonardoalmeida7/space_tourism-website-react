dcb47f871343466130dcae426266d81153778e6f
leonardoalmeida7 <leonardoalmeida2124@gmail.com>
2025-07-11 06:01:50 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,29 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './Pages/Home.jsx';
-import Destination from './Pages/Destination.jsx';
-import Crew from './Pages/Crew.jsx';
+import Home from './Pages/Home.jsx'
+import Destination from './Pages/Destination.jsx'
+import Crew from './Pages/Crew.jsx'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/destination', element: <Destination /> },
+        { path: '/crew', element: <Crew /> },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <App />,
-    children: [
-      { path: '/', element: <Home/> },
-      { path: '/destination', element: <Destination /> },
-      { path: '/crew', element: <Crew /> }
-    ]
-  },
-  {
-    basename: '/space_tourism-website-react',
+    basename: '/space_tourism-website-react', // ESSA LINHA FAZ TUDO FUNCIONAR NO GITHUB PAGES
   }
-])
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 )
