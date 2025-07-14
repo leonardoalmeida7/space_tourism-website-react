57bfa84204ddaf3506afc01288c1bcb70e717de9
leonardoalmeida7 <leonardoalmeida2124@gmail.com>
2025-07-14 06:28:14 -0300

    dando continuidade ao projeto

--- src/Pages/Destination.jsx
+++ src/Pages/Destination.jsx
@@ -1,13 +1,50 @@
-
-import NavDestination from '../Components/Main/NavDestination'
-import  './Destination.css'
+import useFetchData from "../Hooks/useFetchDestinations";
+import { useState } from "react";
+import styles from "./Destination.module.css"
 
 const Destination = () => {
-  return (
-    <div>
-        <NavDestination />
-    </div>
-  )
+  const { data, loading, error } = useFetchData();
+    const [linkIndex, setLinkIndex] = useState(0);
+
+    if (loading) return <p>Carregando...</p>;
+    if (error) return <p>Erro ao carregar os dados.</p>;
+
+    const { destinations, crew, technology } = data;
+    const currentDestination = destinations[linkIndex] || {};
+
+    return (
+      <div className={styles.main}>
+          <p className='page-actual mb-5'>
+              <span className={styles.num}>01</span>
+              PICK YOUR DESTINATION
+          </p>
+          <img className={styles.img}
+          src={currentDestination.images?.png || ''} 
+          alt={currentDestination.name || 'Imagem'} 
+          />
+          <ul className="d-flex justify-content-around px-5">
+          {destinations.map((dest, index) => (
+              <li onClick={() => setLinkIndex(index)} key={index} className={linkIndex == index ? styles.active : styles.link}>
+              {dest.name}
+              </li>
+          ))}
+          </ul>
+          <h1 className={styles.title}>{currentDestination.name}</h1>
+          <p className={styles.description}>{currentDestination.description}
+            <hr />
+          </p>
+          <div className="distance mb-2">
+              <span>AVG. DISTANCE</span>
+              <br />
+              <span className={styles.info}>{currentDestination.distance}</span>
+          </div>
+          <div className="travel-time">
+              <span>EST. TRAVEL TIME</span>
+              <br />
+              <span className={styles.info}>{currentDestination.travel}</span>
+          </div>
+      </div>
+    );
 }
 
 export default Destination
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,23 @@
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
   return (
     <>
-        <main>
-            <span>So, you want to travel to</span>
-            <h1>Space</h1>
-            <p>
-                Let’s face it; if you want to go to space, you might as well genuinely go to
-                outer space and not hover kind of on the edge of it. Well sit back, and relax
-                because we’ll give you a truly out of this world experience!
-            </p>
-            <div>
-                <li>
-                    Explore
-                </li>
+        <main className='text-center d-flex flex-column justify-content-around '>
+            <div className='p-4'>
+                <span>SO, YOU WANT TO TRAVEL TO</span>
+                <h1>SPACE</h1>
+                <p>
+                    Let’s face it; if you want to go to space, you might as well genuinely go to
+                    outer space and not hover kind of on the edge of it. Well sit back, and relax
+                    because we’ll give you a truly out of this world experience!
+                </p>
             </div>
+                <Link className='link' to='/destination'> 
+                    <div className='btn-explore mx-auto'>Explore</div> 
+                </Link>
         </main>
     </>
   )
