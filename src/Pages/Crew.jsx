f985015910ec0d62042a36c591c393409546268d
leonardoalmeida7 <leonardoalmeida2124@gmail.com>
2025-07-15 06:36:39 -0300

    fizalizando dia

--- src/Pages/Crew.jsx
+++ src/Pages/Crew.jsx
@@ -1,5 +1,6 @@
 import useFetchData from '../Hooks/useFetchDestinations'
 import { useState } from 'react';
+import styles from './Crew.module.css'
 
 
 const Crew = () => {
@@ -8,12 +9,46 @@ const Crew = () => {
 
     if (loading) return <p>Carregando...</p>;
     if (error) return <p>Erro ao carregar os dados.</p>;
+    const { destinations, crew, technology } = data;
+    console.log(crew);
   return (
-    <div>
-        <p>
-            <span className='num'>02</span>
+    <div className={styles.main}>
+        <p className={styles.pageActual}>
+            <span className={styles.num}>02</span>
             MEET YOUR CREW
         </p>
+        {crew[linkIndex] && (
+          <div>
+            <div className={styles.info}>
+              <span className={styles.role}>
+                {crew[linkIndex].role}
+              </span>
+              <h1 className={styles.name}>
+                {crew[linkIndex].name}
+              </h1>
+              <p className={styles.bio}>
+                {crew[linkIndex].bio}
+              </p>
+              <div className={styles.circles}>
+              </div>
+            </div>
+            <div>
+              <div className={styles.circles}>
+                {crew.map((member, index) => (
+                  <div  key={index}>
+                    <div onClick={() => setLinkIndex(index)} className={linkIndex === index ? styles.activeCircle : styles.circle}></div>
+                  </div>
+                ))}
+              </div>
+              <div className={styles.imgContainer}>'
+              <img className={styles.img} 
+                src={crew[linkIndex].images?.png || ''} 
+                alt={crew[linkIndex].name || 'Imagem'}
+              /> 
+              </div>
+            </div>
+          </div>
+        )}
     </div>
   )
 }
